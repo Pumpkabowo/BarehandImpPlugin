06e60d329be85175b43bd83aeaca0fa7474efb94
Isabella <68937831+Pumpkabowo@users.noreply.github.com>
2024-12-13 15:08:28 -0700

    Update BarehandImpPlugin.java

--- IimplingHighlight.java
+++ IimplingHighlight.java
@@ -1,97 +1,33 @@
-package net.runelite.client.plugins.implinghighlighter;
+package net.runelite.client.plugins.barehandimp;
 
-import net.runelite.api.Client;
-import net.runelite.api.NPC;
-import net.runelite.api.Skill;
-import net.runelite.api.events.GameTick;
-import net.runelite.api.events.NpcSpawned;
-import net.runelite.api.events.GameObjectSpawned;
-import net.runelite.api.events.GraphicsObjectCreated;
-import net.runelite.client.eventbus.EventBus;
-import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.client.ui.overlay.OverlayManager;
-import net.runelite.client.ui.overlay.Overlay;
-import net.runelite.client.ui.overlay.components.PanelComponent;
+import com.google.inject.Provides; 
 import javax.inject.Inject;
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.util.HashMap;
-import java.util.Map;
 
 @PluginDescriptor(
-        name = "Imp Highlighter"
+        name = "Barehand Imp Highlighter",
+        description = "Highlights imps you can barehand catch based on Hunter level",
+        tags = {"hunter", "imps", "highlight"}
 )
-public class IimplingHighlight extends Plugin
-{
-    private static final Map<String, Integer> IMPLING_LEVELS = new HashMap<>();
-
-    static {
-        IMPLING_LEVELS.put("Baby impling", 27);
-        IMPLING_LEVELS.put("Young impling", 32);
-        IMPLING_LEVELS.put("Gourmet impling", 38);
-        IMPLING_LEVELS.put("Earth impling", 46);
-        IMPLING_LEVELS.put("Essence impling", 52);
-        IMPLING_LEVELS.put("Eclectic impling", 60);
-        IMPLING_LEVELS.put("Nature impling", 68);
-        IMPLING_LEVELS.put("Magpie impling", 75);
-        IMPLING_LEVELS.put("Ninja impling", 84);
-        IMPLING_LEVELS.put("Crystal impling", 90);
-        IMPLING_LEVELS.put("Dragon impling", 93);
-        IMPLING_LEVELS.put("Lucky impling", 99);
-    }
+public class BarehandImpPlugin extends Plugin {
 
     @Inject
-    private Client client;
-
-    @Inject
-    private OverlayManager overlayManager;
-
-    @Inject
-    private ImplingHighlightOverlay overlay;
+    private BarehandImpConfig config;
 
     @Override
-    protected void startUp() throws Exception
-    {
-        overlayManager.add(overlay);
+    protected void startUp() throws Exception {
+        // Plugin startup logic
     }
 
     @Override
-    protected void shutDown() throws Exception
-    {
-        overlayManager.remove(overlay);
+    protected void shutDown() throws Exception {
+        // Plugin shutdown logic
     }
 
-    @Subscribe
-    public void onGameTick(GameTick event)
-    {
-        // Update state on each game tick if needed
-    }
-
-    @Subscribe
-    public void onNpcSpawned(NpcSpawned event)
-    {
-        // Highlight imps here
-    }
-
-    @Subscribe
-    public void onGameObjectSpawned(GameObjectSpawned event)
-    {
-        // Handle game objects if needed
-    }
-
-    @Subscribe
-    public void onGraphicObjectCreated(GraphicsObjectCreated event)
-    {
-        // Handle graphic objects if needed
-    }
-
-    public boolean shouldHighlight(String impName)
-    {
-        int hunterLevel = client.getRealSkillLevel(Skill.HUNTER);
-        Integer requiredLevel = IMPLING_LEVELS.get(impName);
-        return requiredLevel != null && hunterLevel >= requiredLevel;
+    @Provides
+    BarehandImpConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(BarehandImpConfig.class);
     }
 }
